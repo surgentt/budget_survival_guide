0463ba6c07e339d37c155a9efc467090f872d03d
Thomas Surgent <surgentt@gmail.com>
2014-04-09 11:08:57 -0400

    tom updates

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -5,10 +5,23 @@ class Budget < ActiveRecord::Base
   end
 
   def income_after_taxes_calc
-    if self.income != nil
+    if self.income != nil && self.state != nil
       self.income_after_taxes = self.income * (1 - translate_state_to_tax)
       self.save
       self.income_after_taxes
+    end
+  end
+
+  ## Decorators
+  def income_in_months
+    if self.income != nil
+      self.income / 12
+    end
+  end
+
+  def income_after_taxes_in_months
+    if self.income != nil && self.state != nil
+      self.income_after_taxes_calc / 12
     end
   end
 
