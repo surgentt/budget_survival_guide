fa2ddb8610b39a2adb80daf469499beb057e85ca
Thomas Surgent <surgentt@gmail.com>
2014-04-10 17:47:53 -0400

    navbar updates with names

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,4 @@ Rails.application.routes.draw do
       :defaults => {:section => BudgetsController::Sections.first}
   end
 
-
 end
--- spec/controllers/budgets_controller_spec.rb
+++ spec/controllers/budgets_controller_spec.rb
@@ -2,5 +2,38 @@ require 'spec_helper'
 
 describe BudgetsController do
 
+  updated_attributes = {
+    :income => 72000
+  }
+
+  describe "GET #home" do 
+    it "renders the home template" do
+      get :home
+      expect(response).to render_template("home")
+    end
+  end
+
+  describe "GET #edit" do
+    it "renders the :edit template" do 
+      budget = create(:budget)
+      get :edit, :section => "income"
+      expect(response).to render_template("edit")
+    end
+  end
+
+  describe "POST #create" do
+    it "redirect to the budget edit path" do 
+      post :create, budget: attributes_for(:budget)
+      expect(response).to redirect_to budget_edit_path(assigns[:budget])
+    end
+  end
+
+  describe "PUT update" do
+    it 'should redirect the user to the next step' do
+      create(:budget)
+      put :update, :budget => updated_attributes, :section => BudgetsController::Sections.first
+      response.should redirect_to budget_edit_path(:section => BudgetsController::Sections.second)
+    end
+  end
 
 end
