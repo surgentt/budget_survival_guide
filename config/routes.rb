9906aafa257d49fc901de245c975b2bfe0232f2d
brian1987 <pisano.b@gmail.com>
2014-04-10 18:12:41 -0400

    more fixes:

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,8 @@ Rails.application.routes.draw do
   resources :budgets, :except => [:edit] do 
     get 'edit/:section' => 'budgets#edit', :as => 'edit',
       :defaults => {:section => BudgetsController::Sections.first}
-
   end
 
-<<<<<<< HEAD
   match '/about', to: 'budgets#about', via: 'get'
 
-
-
-=======
->>>>>>> 4adf4de6e1cf9d3fe991102fdf71c6c978bbd305
 end
