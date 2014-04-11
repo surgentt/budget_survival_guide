22f2ae3a66efbd8097673e114c05800e8e8db883
Thomas Surgent <surgentt@gmail.com>
2014-04-11 15:14:51 -0400

    don't forget about capybara's

---
+++ spec/features/budget_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+feature 'Budget Creation' do 
+
+  scenario "creates a new budget" do
+    visit root_path
+    click_button "Create Budget"
+    fill_in "budget_income", with: 70000
+    click_button "Next"
+    select('New York', :from => 'budget_state')
+    click_button "Next"
+    fill_in "budget_housing", with: 1325
+    click_button "Next"
+    fill_in "budget_cable_internet", with: 100
+    click_button "Next"
+    fill_in "budget_utilities", with: 150
+    click_button "Next"
+    fill_in "budget_laundry", with: 110
+    click_button "Next"
+    fill_in "budget_commute", with: 110
+    click_button "Next"
+    fill_in "budget_food", with: 110
+    click_button "Next"
+    fill_in "budget_debt_service", with: 110
+    click_button "Next"
+    fill_in "budget_investing", with: 110
+    click_button "Next"
+    fill_in "budget_emergency", with: 110
+    click_button "Next"
+    expect(page).to have_content 'Checkout your budget'
+  end
+
+end
